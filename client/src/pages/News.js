b94077fc412c619a1439f006fc79c49330a13e86
Your NameAlex <alehr45@gmail.com>
2022-02-28 07:24:31 -0600

    footer with social icons and added stock pages

---
+++ client/src/pages/News.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Page from "./Page"
+
+const News = () => {
+  return (
+    <Page title={"News"}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>News</div>
+      </div>
+    </Page>
+  )
+}
+
+export default News
