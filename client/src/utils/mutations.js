88b7a51dca0b54034a92fba80c60ad629c0de9c5
Your NameAlex <alehr45@gmail.com>
2022-03-29 10:03:22 -0500

    merge

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,52 +1,58 @@
 import gql from "graphql-tag"
 
 export const ADD_ADMIN = gql`
-mutation AddAdmin($email: String!, $password: String!) {
+  mutation AddAdmin($email: String!, $password: String!) {
     addAdmin(email: $email, password: $password) {
       token
-      admin{
+      admin {
         _id
       }
     }
-  }`
+  }
+`
 
 export const LOGIN_ADMIN = gql`
   mutation Login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      admin{
+      admin {
         _id
       }
     }
   }
-  `
+`
 
 export const ADD_MUSIC = gql`
-    mutation AddMusic($title: String!, $source: String!) {
+  mutation AddMusic($title: String!, $source: String!) {
     addMusic(title: $title, source: $source) {
       title
       source
     }
   }
-  `
+`
 
 export const ADD_MERCH = gql`
-    mutation AddMerch($name: String!, $description: String!, $quantity: Int!, $price: Float!) {
-    addMerch(name: $name, description: $description, quantity: $quantity, price: $price) {
+  mutation AddMerch($name: String!, $image: String, $type: String!, $quantity: Int!, $price: Float!) {
+    addMerch(name: $name, image: $image, type: $type, quantity: $quantity, price: $price) {
       _id
-      name 
-      description
+      name
+      type
       price
       quantity
+      image
     }
-  }`
+  }
+`
 
 export const UPDATE_MERCH = gql`
-    mutation UpdateMerch($id: ID!, $name: String!, $description: String!, $quantity: Int!, $price: Float!) {
-    updateMerch(_id: $id, name: $name, description: $description, quantity: $quantity, price: $price) {
+  mutation UpdateMerch($id: ID!, $name: String!, $image: String, $type: String!, $quantity: Int!, $price: Float!) {
+    updateMerch(_id: $id, name: $name, image: $image, type: $type, quantity: $quantity, price: $price) {
       _id
-      description
+      type
+      image
       price
       name
+      quantity
     }
-  }`
+  }
+`
