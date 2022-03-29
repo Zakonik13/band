ce6a6b3eb68163a9e9fc3498e4b787dc25182049
w.hunt <wfh2d88@gmail.com>
2022-03-29 09:21:34 -0500

    framework

---
+++ client/src/components/PictureUploader.js
@@ -0,0 +1,80 @@
+import React, { useState, useRef } from "react"
+import $ from "jquery"
+
+const PictureUploader = ({ type, setImage }) => {
+  const inputRef = useRef()
+  const [updateImage] = useMutation(UPDATE_IMAGE)
+  const [src, setSRC] = useState(false)
+  let picture = ""
+
+  const handlePictureSelected = async event => {
+    picture = event.target.files[0]
+    var src = URL.createObjectURL(picture)
+
+    // setPicture(picture);
+    setSRC(src)
+    upload()
+  }
+
+  const renderPreview = () => {
+    if (src) {
+      return (
+        <img
+          alt="Image8"
+          height="auto"
+          width="150
+      "
+          src={src}
+        />
+      )
+    } else {
+      return
+    }
+  }
+
+  const upload = () => {
+    inputRef.current?.click()
+    var formData = new FormData()
+
+    formData.append("image", picture)
+    var result = null
+    $.ajax({
+      url: "https://api.imgur.com/3/image",
+
+      type: "POST",
+      data: formData,
+      headers: {
+        Authorization: "Client-ID 3bd0a7ed5554183"
+      },
+      cache: false,
+      contentType: false,
+      processData: false,
+      async: false,
+      success: function (response) {
+        result = data
+          setImage(response.data.link)
+
+        // Code to handle a succesfull upload
+      }
+    })
+    return result
+  }
+
+  const handleUpload = e => {
+    e.preventDefault()
+    inputRef.current?.click()
+  }
+
+  return (
+    <div className="m-5">
+      <input ref={inputRef} type="file" className="d-none" onChange={handlePictureSelected} />
+
+      <button className="btn btn-success  " onClick={handleUpload}>
+        <div>{renderPreview()}</div>
+        {type === "job" ? "Add Job Image" : "Add Profile Image"}
+      </button>
+    </div>
+  )
+}
+
+export default PictureUploader
