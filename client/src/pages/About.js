b821719aedc1b74a4d3e288e4cfe9e73c86edf68
zakonik <zakonik2417@gmail.com>
2022-04-05 00:01:21 -0400

    commiting for merge

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,17 +1,17 @@
-import React from "react"
-import { GET_ABOUT } from "../utils/queries"
-import { useQuery } from "@apollo/react-hooks"
+import React from "react";
+import { GET_ABOUT } from "../utils/queries";
+import { useQuery } from "@apollo/react-hooks";
 // Components
-import Page from "../components/Page"
+import Page from "../components/Page";
 
 const About = () => {
-  const { data, loading } = useQuery(GET_ABOUT)
+  const { data, loading } = useQuery(GET_ABOUT);
 
   if (loading) {
-    return ""
+    return "";
   }
 
-  console.log(data)
+  console.log(data.about);
 
   return (
     <Page title={"About"}>
@@ -20,16 +20,22 @@ const About = () => {
           display: "flex",
           justifyContent: "center",
           fontSize: 35,
-          fontFamily: "Limo"
+          fontFamily: "Limo",
         }}
       >
         <div>About Us</div>
       </div>
       <hr />
-      <div className="p-5">{data.about[0].body ? <p style={{ textIndent: "50px" }}>{data.about[0].body}</p> : <p style={{ textIndent: "50px" }}>No Data</p>}</div>
+      <div className="p-5">
+        {data.about[0].body ? (
+          <p style={{ textIndent: "50px" }}>{data.about[0].body}</p>
+        ) : (
+          <p style={{ textIndent: "50px" }}>No Data</p>
+        )}
+      </div>
       <hr />
     </Page>
-  )
-}
+  );
+};
 
-export default About
+export default About;
