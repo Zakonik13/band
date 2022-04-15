58fe1aadd64ca342940ea68d22f902adc601c483
zakonik <zakonik2417@gmail.com>
2022-04-15 15:11:58 -0400

    fixed back-button error

--- client/src/components/BackButton.js
+++ client/src/components/BackButton.js
@@ -1,26 +1,27 @@
-import React from "react"
-import back from "../images/back.svg"
+import React from "react";
+import back from "../images/back.svg";
+import { useNavigate } from "react-router-dom";
 
 const BackButton = () => {
-  const goBack = "javascript:history.back()"
+  let navigate = useNavigate();
+
   return (
     <div className="go-back">
       <div>
         {" "}
-        <a href={goBack}>
-          <img
-            alt="back-button"
-            style={{
-              height: "30px",
-              marginLeft: "30px",
-              paddingBottom: "10px"
-            }}
-            src={back}
-          ></img>
-        </a>
+        <img
+          alt="back-button"
+          style={{
+            height: "30px",
+            marginLeft: "30px",
+            paddingBottom: "10px",
+          }}
+          src={back}
+          onClick={() => navigate(-1)}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BackButton
+export default BackButton;
